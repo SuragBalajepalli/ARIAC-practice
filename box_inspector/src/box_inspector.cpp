539831f60009f6629b6d522b0540aabc98756611
SuragBalajepalli <surag.krishna@gmail.com>
2018-04-02 09:23:08 -0400

    Solved for function to sort for misplaced, missing, orphan, and satisfied models

--- box_inspector/src/box_inspector.cpp
+++ box_inspector/src/box_inspector.cpp
@@ -10,6 +10,26 @@ BoxInspector::BoxInspector(ros::NodeHandle* nodehandle) : nh_(*nodehandle) { //c
 
 }
 
+bool BoxInspector::compare_pose(osrf_gear::Model model_A, osrf_gear::Model model_B) {
+ 		Eigen::Affine3d affine1,affine2;
+		Eigen::Vector3d origin_diff;
+		affine1=xformUtils_.transformPoseToEigenAffine3d(model_A.pose);
+		affine2=xformUtils_.transformPoseToEigenAffine3d(model_B.pose);
+		origin_diff = affine1.translation()-affine2.translation();
+		double origin_err = origin_diff.norm();
+		Eigen::Matrix3d R1,R2,R_diff;
+		R1 = affine1.linear();
+		R2 = affine2.linear();
+		R_diff = R1.inverse()*R2;
+		Eigen::AngleAxisd angleAxis(R_diff);
+		double rotation_err = angleAxis.angle();
+  		if(origin_err<0.1 && rotation_err<0.05) {
+  			return 1;
+  		}
+ 	else { return 0; }
+ 	}
+
+
 //to request a new snapshot, set need_new_snapshot_ = true, and make sure to give a ros::spinOnce()
 void BoxInspector::box_camera_callback(const osrf_gear::LogicalCameraImage::ConstPtr & image_msg) {
     if (!got_new_snapshot_) {
@@ -58,19 +78,128 @@ void BoxInspector::get_new_snapshot_from_box_cam() {
   ROS_INFO("got new image");
   int num_parts_seen =  box_inspector_image_.models.size();
   ROS_INFO("update_inspection: box camera saw %d objects",num_parts_seen);
+  int num_parts_desired = desired_models_wrt_world.size();
   orphan_models_wrt_world.clear(); //this will be empty, unless something very odd happens
   satisfied_models_wrt_world.clear();  //shipment will be complete when this matches parts/poses specified in shipment
   misplaced_models_actual_coords_wrt_world.clear();
   misplaced_models_desired_coords_wrt_world.clear();
   missing_models_wrt_world.clear();
+  int num_each_parts_seen[5]={0,0,0,0,0};
+  int num_each_parts_des[5]={0,0,0,0,0};
+  for(int i = 0; i<num_parts_seen;i++) {
+  	switch (mappings[box_inspector_image_.models[i].type]) {
+  		case 1:
+  			num_each_parts_seen[0]+=1;
+  			break;
+  		case 2:
+  			num_each_parts_seen[1]+=1;
+  			break;
+  		case 3:
+  			num_each_parts_seen[2]+=1;
+  			break;
+  		case 4:
+  			num_each_parts_seen[3]+=1;
+  			break;
+  		case 5:
+  			num_each_parts_seen[4]+=1;
+  			break;
+  	}
+  }
+	for(int i = 0; i<num_parts_desired;i++) {
+  		switch (mappings[desired_models_wrt_world[i].type]) {
+  			case 1:
+  				num_each_parts_des[0]+=1;
+  				break;
+  			case 2:
+  				num_each_parts_des[1]+=1;
+  				break;
+  			case 3:
+  				num_each_parts_des[2]+=1;
+  				break;
+  			case 4:
+  				num_each_parts_des[3]+=1;
+  				break;
+  			case 5:
+  				num_each_parts_des[4]+=1;
+  				break;
+  		}
 
   //THIS  IS WRONG...but an example of how to get models from image and sort into category vectors
-  for (int i=0;i<num_parts_seen;i++) {
-     orphan_models_wrt_world.push_back(box_inspector_image_.models[i]);
-  }
-  
+  	}
+	for(int ipart=1;ipart<6;ipart++) {
+		if (num_each_parts_seen[ipart]<num_each_parts_des[ipart]) {
+			for(int j=0;j<desired_models_wrt_world.size();j++) {
+				bool found=false;
+				for(int i=0; i<box_inspector_image_.models.size();i++) {
+					if(box_inspector_image_.models[i].type == desired_models_wrt_world[j].type) {	
+					bool pose_comparison = compare_pose(box_inspector_image_.models[i],desired_models_wrt_world[j]);
+					if (pose_comparison) {
+						found=true;
+					}
+				
+				if (!found) {
+					missing_models_wrt_world.push_back(desired_models_wrt_world[j]);
+				}
+			}
+		}
+	}
+}
+		else if(num_each_parts_seen[ipart]>num_each_parts_des[ipart]) {
+			for(int i=0; i<box_inspector_image_.models.size();i++) {
+				bool found=false;
+				for(int j=0;j<desired_models_wrt_world.size();j++) {
+					if(box_inspector_image_.models[i].type == desired_models_wrt_world[j].type) {
+					bool pose_comparison = compare_pose(box_inspector_image_.models[i],desired_models_wrt_world[j]);
+					if (pose_comparison) {
+						found=true;
+					}
+				
+				if (!found) {
+					orphan_models_wrt_world.push_back(box_inspector_image_.models[i]);
+				}
+			}
+		}
+	}
+}
+		else if(num_each_parts_des[ipart]==num_each_parts_seen[ipart]) {
+			for(int i=0; i<box_inspector_image_.models.size();i++) {
+				bool found=false;
+				for(int j=0;j<desired_models_wrt_world.size();j++) {
+					if(box_inspector_image_.models[i].type == desired_models_wrt_world[j].type) {
+					bool pose_comparison = compare_pose(box_inspector_image_.models[i],desired_models_wrt_world[j]);
+					if (pose_comparison) {
+						found=true;
+					}
+				
+				if (!found) {
+					misplaced_models_actual_coords_wrt_world.push_back(box_inspector_image_.models[i]);
+
+				}
+				
+			}
+		}
+	}
+				for(int i=0;i<desired_models_wrt_world.size();i++){
+				bool found=false;
+				for(int j=0;j<box_inspector_image_.models.size();j++) {
+					if(box_inspector_image_.models[i].type == desired_models_wrt_world[j].type) {
+					bool pose_comparison = compare_pose(box_inspector_image_.models[i],desired_models_wrt_world[j]);
+					if (pose_comparison) {
+						found=true;
+					}
+				
+				if (!found) {
+					misplaced_models_desired_coords_wrt_world.push_back(desired_models_wrt_world[j]);				
+			}
+		}
+	}
+}
+}
+
 
 }
+}
+
 
 //intent of this function is, get a snapshot from the box-inspection camera;
 //parse the image data to see if a shipping box is present
